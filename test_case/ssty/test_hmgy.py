c6800818ae1bd5667d4f19a59677344cdc598e49
Mitang9527 <1129685479@qq.com>
2024-08-28 19:04:58 +0800

    数据库断言初步编写

--- test_case/ssty/test_hmgy.py
+++ test_case/ssty/test_hmgy.py
@@ -1,12 +1,10 @@
 import allure
 import pytest
-from script.hmgy_scipt import src_dir,dst_dir,src_dir_dbc,dst_dir_dbc
-from utils.assertion.assert_control import Assert
-
+from script.hmgy_scipt import src_dir,dst_dir,src_dir_hdc,dst_dir_hdc
 from utils.readFilesUtils.copy_and_modify_files import copy_and_modify_files
 from utils.otherUtils.ConnectServer.ParamikoSSH import clients
+from utils.mysqlUtils.mysqlControl import mysql_db
 from utils.timeUtils.time_control import now_time_day, tomorrow_time_day
-
 
 @allure.epic("wty自动化测试")
 @allure.feature("虎门公园")
@@ -15,12 +13,13 @@ class Testhmgy():
     @pytest.fixture(scope="class")
     #前置连接服务器
     def ssh_client(self):
-        #记录开关打开状态
+
         connected_clients = []
         for client in clients:
             if client.switch:
                 client.connect()
             connected_clients.append(client)
+
 
         yield
         for client in connected_clients:
@@ -30,25 +29,37 @@ class Testhmgy():
     def test_gsc(self,ssh_client):
         #执行虎门公园过山车脚本
         copy_and_modify_files(src_dir, dst_dir,file_ext = "dat",minute_add = 10)
-    @allure.title("收集模块")
+
+    @allure.title("过山车-云端收集")
     def test_gsc_collect(self):
         #从c_video_collect表中获取数据进行断言
-        # TODO 需要做等待,从脚本执行到云端有时间延误
         sql_query = """
         SELECT COUNT(id)
         FROM c_video_collection
-        WHERE create_time BETWEEN '{}' AND '{}'
-        ORDER BY id DESC;
+        WHERE create_time BETWEEN '{}' AND '{}';
         """.format(now_time_day,tomorrow_time_day )
 
-        Assert.sql_switch_handle(self,sql_query)
-    @allure.title("收集模块")
+        expected_result = "待填入的预期结果"
+        mysql_db.wait_for_result(sql_query, expected_result=expected_result)
+
+        result = mysql_db.query(sql_query)  # 执行查询
+        assert result and result[0].get('count') == expected_result #断言
+
+    @allure.title("过山车-云端推理")
+    #从c_inference中获取数据进行断言
+
+    @allure.title("过山车-云端识别")
 
 
-    @allure.story("大摆锤")
+    @allure.title("过山车-云端生成")
+
+
+
+
+    @allure.story("海盗船")
     def test_gsc(self,ssh_client):
         #执行虎门公园大摆锤脚本
-        copy_and_modify_files(src_dir_dbc, dst_dir_dbc,file_ext = "dat",minute_add = 10)
+        copy_and_modify_files(src_dir_hdc, dst_dir_hdc,file_ext = "dat",minute_add = 10)
 
 
 
